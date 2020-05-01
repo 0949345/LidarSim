2d13ac073fad65007d9c89d419867203431595a0
KeanuLin <keanu_jilderda@hotmail.com>
2020-05-01 15:57:07 +0200

    structs geadd

--- lib/GlobalVars/GlobalVars.h
+++ lib/GlobalVars/GlobalVars.h
@@ -1,1 +1,28 @@
 //De globale variable
+#pragma once
+#include <arduino.h>
+#define no_light 1
+#define blind 2
+#define reflector 3
+#define error 4
+#define warning 5
+
+
+
+
+struct headerStruct {
+    int datagram_marker;
+    int protocol;               
+    int version;
+    int length;
+    int fragment_offset;
+
+};
+
+struct beamStruct {
+    int distance;       //in mm
+    int RSSI;           //intensity
+    byte status;        //status = geen licht/blind/reflector/Error/Warning 
+};
+
+beamStruct beams[];
