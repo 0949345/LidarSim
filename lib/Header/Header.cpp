9ba32b2ff1bdd0d9ea4c6cd15567b9f45acffccb
JaydenW <0949345@hr.nl>
2020-05-02 12:56:58 +0200

    Header: ";"

--- lib/Header/Header.cpp
+++ lib/Header/Header.cpp
@@ -5,7 +5,7 @@ void Header::generateFirstHeader(int size) { //biggest part of the header can be
     header.protocol = "MD"; //TO DO: wordt niet goed overgedragen
     header.version = 1.0;
     header.length = size;
-    header.fragment_offset = 0
+    header.fragment_offset = 0;
 }
 void Header::generateNonFirstHeader(int size, int prevSize, int prevOffset) { //biggest part of the header can be hardcoded
     header.datagram_marker = 1; //TO DO: wordt niet goed overgedragen
