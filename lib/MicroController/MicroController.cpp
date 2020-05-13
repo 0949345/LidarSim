f06b6f9793ea5e299610d0e4058fa0845123aa0c
JaydenW <0949345@hr.nl>
2020-05-13 14:36:12 +0200

    small changes

--- lib/MicroController/MicroController.cpp
+++ lib/MicroController/MicroController.cpp
@@ -8,10 +8,10 @@ void MicroController::getData(boolean data[], int counter)
         incomingRawData[i] = data[i];
         Serial.print(data[i]);
     }
-    converter8Bit(data,416);
+    converter8Bit(data, 416);
 }
 
-void MicroController::convertBody(int num,int beamNum)
+void MicroController::convertBody(int num, int beamNum)
 {
     //incomingRawData[n]
     //16    convert van 16 naar distance
@@ -34,7 +34,7 @@ void MicroController::printData()
 {
 }
 
-void MicroController::converter8Bit(boolean data[],int num)
+void MicroController::converter8Bit(boolean data[], int num)
 {
     boolean dataToBeConverted[32];
     int result = 0;
@@ -42,37 +42,31 @@ void MicroController::converter8Bit(boolean data[],int num)
     // {
     //     dataToBeConverted[i] = ;
     // }
-    
 
     for (int i = 0; i < 8; i++)
     {
-        if(data[num+i] == 1){
-            result =+ pow(2,result);
+        if (data[num + i] == 1)
+        {
+            result = +pow(2, result);
         }
     }
-    
+
     Serial.println("result");
     Serial.println(result);
 }
 
-void MicroController::converter16Bit(boolean data[],int num)
+void MicroController::converter16Bit(boolean data[], int num)
 {
     // for (int i = 0; i < 16; i++)
     // {
     //     dataToBeConverted[i] = data[num+i];
     // }
-
-
-
 }
 
-void MicroController::converter32Bit(boolean data[],int num)
+void MicroController::converter32Bit(boolean data[], int num)
 {
     // for (int i = 0; i < 32; i++)
     // {
     //     dataToBeConverted[i] = data[num+i];
     // }
-
-
-
 }
